0b434e92e1be48f21bbe9a38a1c16deaf3c72abb
xiao453177912 <xiao0526vip@163.com>
2018-12-01 17:18:20 +0800

    声明式web服务客户端

---
+++ consumer-feigh/src/main/java/cn/td/controller/HelloController.java
@@ -0,0 +1,18 @@
+package cn.td.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import cn.td.client.EurekaServiceFeign;
+@RestController
+public class HelloController {
+    @Autowired
+    private EurekaServiceFeign eurekaServiceFeign;
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String hello(@PathVariable String name){
+        return eurekaServiceFeign.hello(name);
+    }
+}
